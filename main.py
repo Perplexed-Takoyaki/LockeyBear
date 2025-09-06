3788ed222dea712a32dd8b0b2c81e910f8b1db63
Perplexed-Takoyaki <celiajdcfle@gmail.com>
2025-09-06 17:31:17 +0200

    Initial setup

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+from utils import save_password, retrieve_password
 
 print("Welcome to LockyBear! I am going to help you manage passwords! 🐻")
+
+message_1 = input("type 1 for saving password, 2 for retrieving password")
+
+if message_1 == "1":
+    entity_name = input("What account do you want to save the password of? ")
+    password = input("What is your password for this account? ")
+    save_password(entity_name, password)
+
+elif message_1 == "2":
+    entity_name = input("What account do you want to retrieve the password of? ")
+    retrieve_password(entity_name)
--- utils.py
+++ utils.py
@@ -1,1 +1,24 @@
 import os
+
+def save_password(entity_name, password):
+    file = entity_name + ".passwd"
+    f = open(file, "w")
+    f.write(password)
+    print("Password saved successfully")
+    print(file)
+    f.close()
+
+def retrieve_password(entity_name):
+    file = entity_name + ".passwd"
+    if os.path.exists(file):
+        f = open(file, "r")
+        data = f.read()
+        print("Password retrieved successfully")
+        print(data)
+        f.close()
+        return data
+    else:
+        return "error"
+
+
+
